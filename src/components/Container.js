104d4215770b769c2734668a1b8c1452fce682c5
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-01-26 21:56:03 +0200

    sdd

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
-//import { bpMaxSM } from 'lib/breakpoints'
 
 const Container = ({containerFluid, children}) => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+import Container from './Container'
+import { breakpoints } from '../lib/breakpoints'
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <h1>I'm a header</h1>
+        <Container>
+          <h1
+            css={css`
+              ${breakpoints.xsDown} {
+                display: none;
+              }
+            `}
+          >
+            I'm a header
+          </h1>
+        </Container>
       </header>
     )
   }
